80bb74d77b5330dacbede3b8181494c73b447723
Zhongren Shao <shao.zhongren@gmail.com>
2025-03-12 23:19:40 -0500

    add dummy gpt model

---
+++ chapter4/dummy_gpt_model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)  # calculate token embeddings
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )  # calculate positional embeddings
+        x = tok_embeds + pos_embeds  # add them together
+        x = self.drop_emb(x)  # apply dropout
+        x = self.trf_blocks(x)  # pass through transformer blocks
+        x = self.final_norm(x)  # apply layer normalization
+        logits = self.out_head(x)  # pass through output head
+        return logits
+
+
+class DummyTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+
+class DummyLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+
+    def forward(self, x):
+        return x
---
+++ chapter4/main.py
@@ -0,0 +1,41 @@
+import torch
+from dummy_gpt_model import DummyGPTModel
+
+# 4.1 Coding an LLM architecture
+
+"""
+we have done:
+* tokenization
+* positional encoding
+* multi-head attention
+
+we are going to scale up to GPT-2 size model, with 124 million parameters
+"""
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 1024,  # Context length
+    "emb_dim": 768,  # Embedding dimension
+    "n_heads": 12,  # Number of attention heads
+    "n_layers": 12,  # Number of layers
+    "drop_rate": 0.1,  # Dropout rate
+    "qkv_bias": False,  # Query-Key-Value bias
+}
+
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print(f"Output shape: {logits.shape}")
+print(logits)
