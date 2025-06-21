a01ecf1ceaeb66a9dc097669b16fd741de509201
Zhongren Shao <shao.zhongren@gmail.com>
2025-06-21 11:11:26 -0500

    basic training loop, and see what overfitting looks like

--- chapter5/main.py
+++ chapter5/main.py
@@ -4,7 +4,7 @@ import tiktoken
 
 from chapter4.gpt_model import GPTModel
 from chapter4.util import generate_text_simple
-from .util import text_to_token_ids, token_ids_to_text
+from .util import text_to_token_ids, token_ids_to_text, calc_loss_batch
 
 """
 we will set up our model for text generation and look at basic ways to evaluate the quality of the generated text, and calculate the training and validation losses
@@ -175,3 +175,122 @@ with torch.no_grad():
     val_loss = calc_loss_loader(val_loader, model, device)
 print(f"Train loss: {train_loss}")
 print(f"Validation loss: {val_loss}")
+
+# 5.2 finally training
+
+"""
+steps of loop:
+1. iterate over training epochs (1 epoch is one complete pass over a training set)
+2. iterate over batches in each training epoch
+3. reset loss gradients from previous batch iteration
+4. calculate loss on current batch
+5. backward pass to calculate loss gradients
+6. update model weights using loss gradients
+7. print training and validation set losses
+8. generate sample text for visual inspection
+"""
+
+def train_model_simple(model, train_loader, val_loader, optimizer,
+                       device, num_epochs, eval_freq, eval_iter,
+                       start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch + 1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}"
+                      f"Val loss {val_loss:.3f}")
+
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded,
+            max_new_tokens=50, context_size=context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device)
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=0.0004, weight_decay=0.1
+)
+num_epochs = 10
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs, eval_freq=5, eval_iter=5,
+    start_context="Every effort moves you", tokenizer=tokenizer
+)
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+matplotlib.use("Agg")
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(
+        epochs_seen, val_losses, linestyle="-.", label="Validation loss"
+    )
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    # plt.show()
+    plt.savefig("output.png", dpi=300, bbox_inches="tight")
+    plt.close()
+
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+"""
+here, training and validation loss diverge because the model is overfitting to the training data
+this is expected because the training dataset is very small, and we are doing multiple epochs
+usually, we do a much larger dataset for only one epoch
+"""
+
